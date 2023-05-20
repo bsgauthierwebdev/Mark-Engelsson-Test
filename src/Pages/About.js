a0b43e535407462cbac0ad06f236d141fa9334a0
Brent Gauthier <brentsgauthier@gmail.com>
2023-05-19 20:41:01 -0400

    Build & Style About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import '../Styles/About.css'
 
+const GET_AUTHOR = gql`
+  query AuthorInfo {
+    authors {
+      bio
+      name
+      photo {
+        url
+      }
+    }
+  }
+`
 const About = () => {
-  return (
-    <div>
-        <h1>About</h1>
+  const { loading, error, data } = useQuery(GET_AUTHOR)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+  console.log(data)
+
+  return data.authors.map((
+    {name, photo, bio}
+  ) => (
+    <div className = 'About'>
+      <div className = 'About-container'>
+        <div className = 'About-container-name'>
+          {/* <h1>{name}</h1> */}
+          <h1>About {name}</h1>
+          <h2>The guy in the photo</h2>
+          <h3>Behind the book</h3>
+          <p>(He writes other stuff too, just you wait...)</p>
+        </div>
+        <div className = 'About-container-content'>
+          <div className = 'About-container-photo'>
+            <img src = {photo.url} alt = {name} />
+          </div>
+          <div className = 'About-container-bio'>
+            {bio}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  ))
 }
 
-export default About
+export default About 
