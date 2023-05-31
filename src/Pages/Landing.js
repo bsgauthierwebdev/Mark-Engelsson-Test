ac1e26f15264b4afb685f234a0d0d0de61dce236
Brent Gauthier <cajundrummer@gmail.com>
2023-05-30 21:14:54 -0400

    Used as styling template, will change later

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import '../Styles/Landing.scss'
+
+const GET_BOOK_CARD = gql`
+  query GetBooks {
+    books {
+        title
+        description
+        purchaseLink
+        cover {
+            url
+        }
+    }
+  }
+`
 
 const Landing = () => {
   return (
-    <div>
-        <h1>Landing</h1>
+    <div className = 'Landing'>
+        <header className = 'Landing-header'><h2>TITLE title TITLE</h2></header>
+        <div className = 'Landing-content'>
+          <div className = 'Landing-content-image'>
+            <img 
+              src = 'https://pbs.twimg.com/media/CvxPO66XEAAhTLX.jpg:large' 
+              
+            />
+          </div>
+          <div className = 'Landing-content-text'>
+            <p>This is where all of the text will be placed</p>
+          </div>
+        </div>
     </div>
   )
 }
