631319453eeac6b4a57ce1749ae036873c6f959d
Brent Gauthier <cajundrummer@gmail.com>
2023-06-07 11:44:43 -0400

    Building and styling Press page

--- src/Pages/Press.js
+++ src/Pages/Press.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import '../Styles/Press.css'
+
+const GET_PRESS = gql`
+  query GetPressDetails {
+    presses {
+      name
+      organization
+      quote
+    }
+  }
+`
 
 const Press = () => {
-  return (
-    <div>
-        <h1>Press</h1>
+  const {loading, error, data} = useQuery(GET_PRESS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+  
+  console.log(data)
+
+  return data.presses.map((
+    {
+      quote,
+      name,
+      organization,
+    }
+  ) => (
+    <div className = 'Press'>
+      <div className = 'text-bubble'>
+        {quote}
+      </div>
+      <div className = 'contributor'>
+        <p>{name}</p>
+        <p>{organization}</p>
+      </div>
     </div>
-  )
+  ))
+
 }
 
 export default Press
