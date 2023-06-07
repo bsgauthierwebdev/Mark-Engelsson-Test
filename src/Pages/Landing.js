40f8944acab8c13e5ed0fa6016d0003107a8affa
Brent Gauthier <cajundrummer@gmail.com>
2023-06-07 16:09:58 -0400

    Landing complete

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,37 +1,40 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client'
+import headshot from '../Images/headshot.jpeg'
+import cover from '../Images/wiwimimcover.jpg'
 import '../Styles/Landing.scss'
-
-const GET_BOOK_CARD = gql`
-  query GetBooks {
-    books {
-        title
-        description
-        purchaseLink
-        cover {
-            url
-        }
-    }
-  }
-`
 
 const Landing = () => {
   return (
     <div className = 'Landing'>
-        <header className = 'Landing-header'><h2>TITLE title TITLE</h2></header>
-        <div className = 'Landing-content'>
-          <div className = 'Landing-content-image'>
-            <img 
-              src = 'https://pbs.twimg.com/media/CvxPO66XEAAhTLX.jpg:large' 
-              
-            />
+      <div className = 'Landing-intro'>
+        <div className = 'Landing-intro-name'>
+          <p>Hey everyone! I'm Mark!</p>
+        </div>
+        <div className = 'Landing-intro-image'>
+          <img src = {headshot} />
+        </div>
+      </div>
+      <div className = 'Landing-description'>
+        <p>Writer | Poet</p>
+      </div>
+      <div className = 'Landing-minibio'>
+        <div className = 'minibio-cover'>
+          <img 
+            src = {cover}
+            alt = 'book'
+          />
+        </div>
+        <div className = 'minibio-description'>
+          <div className = 'minibio-description-quote'>
+            <p>Mark Engelsson is a transatlantic nomad masquerading as a writer. He mostly focuses on poetry, with occasional forays into short stories and a couple of ideas for novels on the back-burner. What If What’s Inside Me Isn’t Me? is his début pamphlet, exploring the duality in life while being unafraid to shine a light on the darker side of humanity.</p>
           </div>
-          <div className = 'Landing-content-text'>
-            <p>This is where all of the text will be placed</p>
+          <div className = 'minibio-description-source'>
+            <p>- Amazon</p>
           </div>
         </div>
+      </div>
     </div>
   )
 }
 
-export default Landing
+export default Landing 
