9e3a0db2c351aa8ea7c4d7accdee910ac770ae0d
Brent Gauthier <cajundrummer@gmail.com>
2023-06-07 22:28:36 -0400

    Building comments form

--- src/Components/CommentsForm.jsx
+++ src/Components/CommentsForm.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
+import '../Styles/CommentsForm.css'
 
-const CommentsForm = () => {
+export const submitComment = async (obj) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    body: JSON.stringify(obj)
+  })
+
+  return result.json()
+}
+
+const CommentsForm = ({slug}) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
+  const handleCommentSubmit = () => {
+    setError(false)
+
+    const {value: comment} = commentEl.current
+    const {value: name} = nameEl.current
+    const {value: email} = emailEl.current
+    const {checked: storeData} = storeDataEl.current
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = {name, email, comment, slug}
+
+    if (storeData) {
+      localStorage.setItem('name', name)
+      localStorage.setItem('email', email)
+    } else {
+      localStorage.removeItem('name', name)
+      localStorage.removeItem('email', email)
+    }
+
+  }
+
   return (
-    <div>
-        <h1>CommentsForm</h1>
+    <div className = 'CommentsForm'>
+        <h3 className = 'CommentsForm-header'>CommentsForm</h3>
+        <div className = 'CommentsForm-comment'>
+          <textarea 
+            ref = {commentEl} 
+            className = 'comment-container'
+            placeholder = 'Leave your comments here...'
+            name = 'comment'
+          />
+        </div>
+        <div className = 'CommentsForm-sender'>
+          <input 
+            type = 'text'
+            ref = {nameEl}
+            className = 'name-container'
+            placeholder = 'Name'
+            name = 'name'
+          />
+          <input 
+              type = 'text'
+              ref = {emailEl}
+              className = 'email-container'
+              placeholder = 'Email'
+              name = 'email'
+            />
+        </div>
+        <div className = 'save-info'>
+          <div>
+            <input ref = {storeDataEl} type = 'checkbox' id = 'storeData' name = 'storeData' value = 'true' />
+            <label className = 'save-info-label' htmlFor = 'storeData'>Save my info</label>
+          </div>
+        </div>
+        {error && <p className = 'invalid-input'>All fields are required</p>}
+        <div className = 'comment-submit'>
+          <button 
+            type = 'button' 
+            onClick = {handleCommentSubmit}
+            className = 'comment-button'
+          >
+            Submit Comment  
+          </button>
+          {showSuccessMessage && <span className = 'submit-message'>Comment submitted for review...</span>}
+        </div>
     </div>
   )
 }
