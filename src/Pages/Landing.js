f6f51480b990c417ef0ec342a8de89605e80e2e2
Brent Gauthier <cajundrummer@gmail.com>
2024-02-12 10:04:18 -0500

    Update Landing Page Text

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -11,32 +11,31 @@ const Landing = () => {
     <div className = 'Landing'>
       <div className = 'Landing-intro'>
         <div className = 'Landing-intro-name'>
-          <div className = 'Landing-intro-name-1'>
             <p>Hey everyone!</p>
-          </div>
-          <div className = 'Landing-intro-name-2'>
-            <p>I'm Mark!</p>
-          </div>
+            <p>I'm Mark, </p>
+            <p>welcome to my world.</p>
         </div>
         <div className = 'Landing-intro-image'>
           <img src = {headshot3} alt = 'headshot' />
         </div>
       </div>
-      <div className = 'Landing-description'>
-        <p>Writer | Poet</p>
-      </div>
       <div className = 'Landing-minibio'>
         <div className = 'minibio-cover'>
           <Link to = 'https://www.amazon.co.uk/What-If-Whats-Inside-Isnt-ebook/dp/B0BST82LXY/' target = '_blank'>
             <img 
-              src = {cover}
+              src = {headshot}
               alt = 'book'
             />
           </Link>
         </div>
         <div className = 'minibio-description'>
           <div className = 'minibio-description-quote'>
-            <p>Mark Engelsson is a transatlantic nomad masquerading as a writer. He mostly focuses on poetry, with occasional forays into short stories and a couple of ideas for novels on the back-burner. What If What’s Inside Me Isn’t Me? is his début pamphlet, exploring the duality in life while being unafraid to shine a light on the darker side of humanity.</p>
+            <p>
+              Make yourself at home. You can learn about my journey, check out my books, and see some nice things people said about what I've written. I’ll also update the Press page when I’m going to events, so you can say hi! There’s also a blog with various pieces of my writing, musings and advice for fellow creatives, so keep an eye out there.
+            </p>
+            <p>
+              If you want to drop me a line, hit the contact page (not too hard, please). To stay ahead of the curve with exciting announcements (plus exclusive access to discounts and sneak peeks at fresh writing/works in progress), sign up for my newsletter! I also put links to the blog posts in the newsletter, so don't miss out on the fun.
+            </p>
           </div>
           {/* <div className = 'minibio-description-source'>
             <p>- Amazon</p>
